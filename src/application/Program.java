c1547394f718dee68187a4a1be7e39946eaa2c71
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2023-06-20 18:41:12 -0300

    Implementando Herança

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Shape s1 = new Circle(Colors.valueOf("BLACK"), 2.0);
-		Shape s2 = new Retangle(Colors.valueOf("RED"), 200, 500);
+		Shape s2 = new Retangle(Colors.valueOf("RED"), 4, 5);
 
 		System.out.println("Circulo: " + s1);
 		System.out.println("Retangulo: " + s2);
